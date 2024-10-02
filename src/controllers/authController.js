32790372b85a84969d5a35bcc6b3c622026ad266
tinithk <tinitse@gmail.com>
2024-10-02 22:54:34 +0530

    catchAsync util is developed to catch the async errors and call global error handler in central place

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,19 +1,32 @@
 import { register } from "../services/authService.js";
+import catchAsync from "../util/catchAsync.js";
 
-export const signUp = async (req, res, next) => {
-  try {
-    console.log(req.body);
-    const response = await register(req.body);
-    if (response) {
-      const { user, accessToken, refreshToken } = response;
-      res
-        .status(200)
-        .json({
-          message: "success",
-          data: { user, accessToken, refreshToken },
-        });
-    }
-  } catch (error) {
-    next(error);
+// export const signUp = async (req, res, next) => {
+//   try {
+//     console.log(req.body);
+//     const response = await register(req.body);
+//     if (response) {
+//       const { user, accessToken, refreshToken } = response;
+//       res
+//         .status(200)
+//         .json({
+//           message: "success",
+//           data: { user, accessToken, refreshToken },
+//         });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+export const signUp = catchAsync(async (req, res, next) => {
+  const response = await register(req.body);
+  if (response) {
+    const { user, accessToken, refreshToken } = response;
+    console.log(user);
+    res.status(200).json({
+      message: "success",
+      data: { user, accessToken, refreshToken },
+    });
   }
-};
+});
