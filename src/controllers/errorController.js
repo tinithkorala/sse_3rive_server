74100452749f7f25d907a3a0d66a109841d82575
tinithk <tinitse@gmail.com>
2024-10-04 00:01:45 +0530

    globalErrorHandler modified to send error based on the environment; and use winston logger to log the error;

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,3 +1,30 @@
+import logger from "../util/logger.js";
+
+const sendErrorResDev = (error, res) => {
+  res.status(error.statusCode).json({
+    name: error.name,
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+const sendErrorResProd = (error, res) => {
+  if (error.isOperational) {
+    // Tursted errors
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    // Unknown errors
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
 const globalErrorHandler = (error, req, res, next) => {
   const statusCode = error.statusCode || 500;
   const status = error.status || "error";
@@ -5,12 +32,13 @@ const globalErrorHandler = (error, req, res, next) => {
   const name = error.name || "Error";
   const stack = error.stack || "";
 
-  res.status(statusCode).json({
-    name,
-    status,
-    message,
-    stack
-  });
-}
+  logger.error({ message, stack });
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorResDev(error, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorResProd(error, res);
+  }
+};
 
 export default globalErrorHandler;
