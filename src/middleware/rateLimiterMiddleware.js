391376e4cbd897153c73c390209b75bbc8f8c727
tinithk <tinitse@gmail.com>
2024-10-04 13:45:29 +0530

    logRateLimitError response formatted and log the message to log-file to check which ip exceed the rate limiters for which routes

--- src/middleware/rateLimiterMiddleware.js
+++ src/middleware/rateLimiterMiddleware.js
@@ -1,14 +1,28 @@
 import rateLimit from "express-rate-limit";
 
 import { rateLimitConfig } from "../config/appConfig.js";
+import logger from "../util/logger.js";
 
 const { max, windowMs } = rateLimitConfig;
+
+const logRateLimitError = (req, res, next, options) => {
+  const { ip, method, originalUrl } = req;
+  const message = `Rate limit exceeded: ${ip} tried to ${method} ${originalUrl}`;
+  logger.error({message, stack: ''})
+  res.status(options.statusCode).json({
+    status: "fail",
+    message: options.message || "Too many requests, please try again later.",
+  });
+};
 
 const rateLimiterMiddleware = rateLimit({
   max,
   windowMs,
   message: "Too many requests from this IP, please try again later.",
   headers: true,
+  handler: (req, res, next, options) => {
+    logRateLimitError(req, res, next, options);
+  },
 });
 
 export default rateLimiterMiddleware;
